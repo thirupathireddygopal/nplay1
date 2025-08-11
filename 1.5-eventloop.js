502a7964d200f9ad68601ce21a3a0069cab158cb
Thirupathireddy Gopal <thirupathireddygopal@gmail.com>
2025-08-11 15:11:02 +0530

    up chgs

--- 1.5-eventloop.js
+++ 1.5-eventloop.js
@@ -1,6 +1,6 @@
 // synchronous running
 // return undefined when we don't return anything
-console.log('======== 1.console ========');
+console.log('======== console ========');
 console.log('start1');
 function a() {
     console.log('hi thiru, js engine makes a request to get it print on to the console...');
@@ -9,21 +9,21 @@ a();
 console.log('end1');
 
 // js engine registers callbacks in web-apis enviorment and pushing that callback to callstack using eventloop fom callback queue
-console.log('======== 2.event loop using callbacks ========');
+console.log('======== 1.event loop using callbacks ========');
 console.log('start2');
 setTimeout(function cbTimer() {
     console.log('this callback timer executed after 2 secs');
 }, 2000);
 console.log('end2');
 
-console.log('======== 3.event loop using eventListeners ========');
+console.log('======== 2.event loop using eventListeners ========');
 console.log('start3');
 document.getElementById('clickMe').addEventListener('click', function cbEvent() {
     console.log('this event listener callback executes after click event....')
 })
 console.log('end3');
 
-console.log('======== 4.event loop using fetch ========');
+console.log('======== 3.event loop using fetch ========');
 console.log('start4');
 setTimeout(function cbT2() {
     console.log('executed after 4 secs');
