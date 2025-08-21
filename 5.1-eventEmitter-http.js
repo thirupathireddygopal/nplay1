ade61a64fa0b15be7abcdf39f44164d4d006a5be
Thirupathireddy Gopal <thirupathireddygopal@gmail.com>
2025-08-21 19:32:35 +0530

    up chgs

--- 5.1-eventEmitter-http.js
+++ 5.1-eventEmitter-http.js
@@ -5,13 +5,59 @@
 // ex: HTTP Server
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.end('Hello World');
+    res.end('Hello World'); // sending the response
 });
 
-server.on('request', (req, res) => {
-    console.log('Request received'); // prints, when we make a GET request http://localhost:3000/
+server.on('request', (req, res) => { // listening requests
+    console.log('Request received for:', req.url); // prints, when we make a GET request http://localhost:3000/
 });
 server.listen(3000, () => {
     console.log('server listening on port: ', 3000)
 });
 
+
+/**
+if (req.url === '/favicon.ico') {
+    // serve a small favicon
+    const faviconPath = path.join(__dirname, 'favicon.ico');
+    fs.readFile(faviconPath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end();
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+      res.end(data);
+    });
+  } else if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+ */
+
+/**
+ * The reason is: it’s not your code, it’s the browser.
+ * When you type http://localhost:3000/ in Chrome/Firefox/Edge, 
+ * the browser always tries to fetch a favicon (the little icon that shows up in the browser tab).
+    const http = require('http');
+    const server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+    // ignore favicon requests
+    res.writeHead(204); // No Content
+    res.end();
+    return;
+    }
+
+    console.log('Request received for:', req.url);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+    });
+
+    server.listen(3000, () => {
+    console.log('Server listening on port: 3000');
+    });
+
+*/
